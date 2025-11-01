f0434b3e75339f0c563ac3cd96db40bf0d59fed5
mugu1997 <nmugunthan152@gmail.com>
2025-11-01 10:20:53 +0530

    new menu changes

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -3,12 +3,12 @@
         <ul class="nav">
             <!-- @can('access_report')
     <li class="nav-item">
-                                        <a href="{{ route('admin.report.index') }}"
-                                            class="nav-link {{ request()->is('admin/report') || request()->is('admin/report/*') ? 'active' : '' }}">
-                                            <i class="fi fi-rr-apps nav-icon"></i>
-                                            {{ trans('cruds.report.title') }}
-                                        </a>
-                                    </li>
+                                                <a href="{{ route('admin.report.index') }}"
+                                                    class="nav-link {{ request()->is('admin/report') || request()->is('admin/report/*') ? 'active' : '' }}">
+                                                    <i class="fi fi-rr-apps nav-icon"></i>
+                                                    {{ trans('cruds.report.title') }}
+                                                </a>
+                                            </li>
 @endcan -->
             @can('user_manage_access')
                 <li class="nav-item nav-dropdown">
@@ -77,7 +77,7 @@
                     </a>
                 </li>
             @endcan
-              @if(Gate::check('dairy_access') || Gate::check('region_access'))
+            @if (Gate::check('dairy_access') || Gate::check('region_access'))
                 <li class="nav-item nav-dropdown">
                     <a class="nav-link nav-dropdown-toggle" href="#">
                         <i class="fi fi-br-home-location-alt nav-icon"></i>
@@ -107,7 +107,7 @@
                 </li>
             @endif
 
-            @if(Gate::check('product_access') || Gate::check('vendor_access'))
+            @if (Gate::check('product_access') || Gate::check('vendor_access'))
                 <li class="nav-item nav-dropdown">
                     <a class="nav-link nav-dropdown-toggle" href="#">
                         <i class="fi fi-br-box nav-icon"></i>
@@ -124,59 +124,60 @@
                                 </li>
                             @endcan
 
-            @can('vendor_access')
-                <li class="nav-item">
-                    <a href="{{ route('admin.vendors.index') }}"
-                        class="nav-link {{ request()->is('admin/vendors') || request()->is('admin/vendors/*') ? 'active' : '' }}">
-                        <i class="fi fi-br-briefcase nav-icon"></i>
-                        Vendors
-                    </a>
-                </li>
-            @endcan
+                            @can('vendor_access')
+                                <li class="nav-item">
+                                    <a href="{{ route('admin.vendors.index') }}"
+                                        class="nav-link {{ request()->is('admin/vendors') || request()->is('admin/vendors/*') ? 'active' : '' }}">
+                                        <i class="fi fi-br-briefcase nav-icon"></i>
+                                        Vendors
+                                    </a>
+                                </li>
+                            @endcan
 
-            @can('product_access')
-                <li class="nav-item">
-                    <a href="{{ route('admin.products.index') }}"
-                        class="nav-link {{ request()->is('admin/products') || request()->is('admin/products/*') ? 'active' : '' }}">
-                        <i class="fi fi-br-box nav-icon"></i>
-                        Products
-                    </a>
-                </li>
-            @endcan
+                            @can('product_access')
+                                <li class="nav-item">
+                                    <a href="{{ route('admin.products.index') }}"
+                                        class="nav-link {{ request()->is('admin/products') || request()->is('admin/products/*') ? 'active' : '' }}">
+                                        <i class="fi fi-br-box nav-icon"></i>
+                                        Products
+                                    </a>
+                                </li>
+                            @endcan
 
-            @can('expensecategory_access')
-                <li class="nav-item">
-                    <a href="{{ route('admin.expense_categories.index') }}"
-                        class="nav-link {{ request()->is('admin/expense_categories') || request()->is('admin/expense_categories/*') ? 'active' : '' }}">
-                        <i class="fi fi-br-box nav-icon"></i>
-                        Expense Category
-                    </a>
-                </li>
-            @endcan
+                            @can('expensecategory_access')
+                                <li class="nav-item">
+                                    <a href="{{ route('admin.expense_categories.index') }}"
+                                        class="nav-link {{ request()->is('admin/expense_categories') || request()->is('admin/expense_categories/*') ? 'active' : '' }}">
+                                        <i class="fi fi-br-box nav-icon"></i>
+                                        Expense Category
+                                    </a>
+                                </li>
+                            @endcan
 
-            @can('fundallocation_access')
-                <li class="nav-item">
-                    <a href="{{ route('admin.fund_allocations.index') }}"
-                        class="nav-link {{ request()->is('admin/fund_allocations*') ? 'active' : '' }}">
-                        <i class="fi fi-br-piggy-bank nav-icon"></i>
-                        Fund Allocation
-                    </a>
-                </li>
-            @endcan
+                            @can('fundallocation_access')
+                                <li class="nav-item">
+                                    <a href="{{ route('admin.fund_allocations.index') }}"
+                                        class="nav-link {{ request()->is('admin/fund_allocations*') ? 'active' : '' }}">
+                                        <i class="fi fi-br-piggy-bank nav-icon"></i>
+                                        Fund Allocation
+                                    </a>
+                                </li>
+                            @endcan
 
 
-            @if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php')))
-                @can('profile_password_edit')
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->is('profile/password*') ? 'active' : '' }}"
-                            href="{{ route('profile.password.edit') }}">
-                            <i class="fi fi-br-lock nav-icon"></i>
-                            {{ trans('global.change_password') }}
-                        </a>
-                    </li>
-                @endcan
+                            @if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php')))
+                                @can('profile_password_edit')
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ request()->is('profile/password*') ? 'active' : '' }}"
+                                            href="{{ route('profile.password.edit') }}">
+                                            <i class="fi fi-br-lock nav-icon"></i>
+                                            {{ trans('global.change_password') }}
+                                        </a>
+                                    </li>
+                                @endcan
+                            @endif
+
+                    </ul>
             @endif
-
-        </ul>
     </nav>
 </div>
